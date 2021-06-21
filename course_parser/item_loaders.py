4191a819563baf5fc0a68e36a775e3f00e65b4dd
J00N5 <fnbdy+bitbucket.work@icloud.com>
2021-06-21 20:51:34 +0800

    Fix

--- course_parser/item_loaders.py
+++ course_parser/item_loaders.py
@@ -1,43 +1,43 @@
-# from os import replace
-# from itemloaders.processors import (Compose, Identity, Join, MapCompose,
-#                                     TakeFirst)
-# from scrapy.loader import ItemLoader
+from os import replace
+from itemloaders.processors import (Compose, Identity, Join, MapCompose,
+                                    TakeFirst)
+from scrapy.loader import ItemLoader
 
-# from .utils import text_to_int
-
-
-# class CourseLoader(ItemLoader):
-#     default_input_processor = MapCompose(str.strip)
-#     default_output_processor = TakeFirst()
-
-#     def is_GE(ge_text: str) -> bool:
-#         return ge_text == 'General Education Class'
-
-#     def is_program_applicable(program_applicable: str):
-#         return not program_applicable.startswith('Not')
-
-#     def is_credit_degree_applicable(credit_degree_applicable: str):
-#         return not credit_degree_applicable.startswith('Non')
-
-#     def remove_spaces(s: str):
-#         return s.replace(' ', '')
-
-#     GE_in = MapCompose(default_input_processor, is_GE)
-
-#     program_applicable_in = MapCompose(
-#         default_input_processor, is_program_applicable)
-
-#     credit_degree_applicable_in = MapCompose(
-#         default_input_processor, is_credit_degree_applicable)
-
-#     hours_in = MapCompose(default_input_processor, text_to_int)
-
-#     notes_out = Identity()
-
-#     # dept_code_in = MapCompose(default_input_processor, remove_spaces)
+from .utils import text_to_int
 
 
-# class DepartmentLoader(ItemLoader):
+class CourseLoader(ItemLoader):
+    default_input_processor = MapCompose(str.strip)
+    default_output_processor = TakeFirst()
 
-#     default_input_processor = MapCompose(str.strip)
-#     default_output_processor = TakeFirst()
+    def is_GE(ge_text: str) -> bool:
+        return ge_text == 'General Education Class'
+
+    def is_program_applicable(program_applicable: str):
+        return not program_applicable.startswith('Not')
+
+    def is_credit_degree_applicable(credit_degree_applicable: str):
+        return not credit_degree_applicable.startswith('Non')
+
+    def remove_spaces(s: str):
+        return s.replace(' ', '')
+
+    GE_in = MapCompose(default_input_processor, is_GE)
+
+    program_applicable_in = MapCompose(
+        default_input_processor, is_program_applicable)
+
+    credit_degree_applicable_in = MapCompose(
+        default_input_processor, is_credit_degree_applicable)
+
+    hours_in = MapCompose(default_input_processor, text_to_int)
+
+    notes_out = Identity()
+
+    # dept_code_in = MapCompose(default_input_processor, remove_spaces)
+
+
+class DepartmentLoader(ItemLoader):
+
+    default_input_processor = MapCompose(str.strip)
+    default_output_processor = TakeFirst()
--- course_parser/pipelines.py
+++ course_parser/pipelines.py
@@ -17,60 +17,60 @@ from colorama import Style, Fore
 from . import models
 
 
-class CoursePipeline:
+# class CoursePipeline:
 
-    def __init__(self):
-        """
-        Initializes database connection and sessionmaker
-        Creates tables
-        """
-        engine = models.db_connect()
-        models.create_table(engine)
-        self.Session = sessionmaker(bind=engine)
+#     def __init__(self):
+#         """
+#         Initializes database connection and sessionmaker
+#         Creates tables
+#         """
+#         engine = models.db_connect()
+#         models.create_table(engine)
+#         self.Session = sessionmaker(bind=engine)
 
-    def process_item(self, item, spider):
-        if isinstance(item, Course) or isinstance(item, Department):
-            session = self.Session()
+#     def process_item(self, item, spider):
+#         if isinstance(item, Course) or isinstance(item, Department):
+#             session = self.Session()
 
-            if isinstance(item, Course):
-                course = models.Course()
+#             if isinstance(item, Course):
+#                 course = models.Course()
 
-                course.id = item.get('id', '')
-                course.name = item.get('name', '')
-                # course.dept_code = item.get('dept_code', '')
-                course.description = item.get('description', '')
-                course.units = item.get('units', '')
-                course.hours = item.get('hours', '')
-                course.total_hours = item.get('total_hours', '')
-                course.GE = item.get('GE', '')
-                course.program_applicable = item.get('program_applicable', '')
-                course.credit_degree_applicable = item.get(
-                    'credit_degree_applicable', '')
-                course.grading_method = item.get('grading_method', '')
-                course.prereqs = item.get('prereqs', '')
-                course.advisory = item.get('advisory', '')
-                course.notes = item.get('notes', '')
-                course.coreqs = item.get('coreqs', '')
-                course.details_page = item.get('details_page', '')
-                course.outline_page = item.get('outline_page', '')
-                course.last_updated = datetime.now()
+#                 course.id = item.get('id', '')
+#                 course.name = item.get('name', '')
+#                 # course.dept_code = item.get('dept_code', '')
+#                 course.description = item.get('description', '')
+#                 course.units = item.get('units', '')
+#                 course.hours = item.get('hours', '')
+#                 course.total_hours = item.get('total_hours', '')
+#                 course.GE = item.get('GE', '')
+#                 course.program_applicable = item.get('program_applicable', '')
+#                 course.credit_degree_applicable = item.get(
+#                     'credit_degree_applicable', '')
+#                 course.grading_method = item.get('grading_method', '')
+#                 course.prereqs = item.get('prereqs', '')
+#                 course.advisory = item.get('advisory', '')
+#                 course.notes = item.get('notes', '')
+#                 course.coreqs = item.get('coreqs', '')
+#                 course.details_page = item.get('details_page', '')
+#                 course.outline_page = item.get('outline_page', '')
+#                 course.last_updated = datetime.now()
 
-                session.add(course)
+#                 session.add(course)
 
-            elif isinstance(item, Department):
-                dept = models.Department()
+#             elif isinstance(item, Department):
+#                 dept = models.Department()
 
-                dept.name = item.get('name', '')
-                dept.code = item.get('code', '')
-                dept.id = item.get('id', '')
-                dept.loc = item.get('loc', '')
-                dept.phone = item.get('phone', '')
-                dept.home_page = item.get('home_page', '')
-                dept.last_updated = datetime.now()
+#                 dept.name = item.get('name', '')
+#                 dept.code = item.get('code', '')
+#                 dept.id = item.get('id', '')
+#                 dept.loc = item.get('loc', '')
+#                 dept.phone = item.get('phone', '')
+#                 dept.home_page = item.get('home_page', '')
+#                 dept.last_updated = datetime.now()
 
-                session.add(dept)
+#                 session.add(dept)
 
-            session.commit()
-            session.close()
+#             session.commit()
+#             session.close()
 
-        return item
+#         return item
